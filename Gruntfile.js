7542367497b5858ffe76afee7b73d4dcd70b8e46
Chareesa <chareesagraham@gmail.com>
2014-12-01 12:11:24 -0800

    browserified

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,47 @@
 'use strict';
 
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.loadNpmTasks('grunt-jscs');
 
   grunt.initConfig({
+    clean: {
+      dev: {
+        src: ['build/']
+      }
+    },
+
+    copy: {
+      dev: {
+        cwd: 'app/',
+        src: ['**/*.html', 'css/**/*.css'],
+        expand: true,
+        dest: 'build/'
+      }
+    },
+
+    browserify: {
+      dev: {
+        src: ['app/js/**/*.js'],
+        dest: 'build/bundle.js',
+        options: {
+          transform: ['debowerify']
+        }
+      }
+    },
+
+    test: {
+      src: ['test/client/**/*test.js'],
+      dest: 'test/test_bundle.js',
+      options: {
+        transform: ['debowerify']
+      }
+    },
+
     jshint: {
       options: {
         node: true
@@ -25,6 +61,6 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('test', ['jshint', 'simplemocha', 'jscs']);
+  grunt.registerTask('build:dev', ['clean:dev', 'browserify:dev', 'copy:dev', 'jshint', 'simplemocha', 'jscs']);
   grunt.registerTask('default', ['test']);
 };
